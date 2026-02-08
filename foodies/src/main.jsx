62e8b3f792cab44bb5cd3a02dbfc4b5c641ba5d1
K.M.Suranga Madushan Kulathunga <205887870+SurangaMadushan@users.noreply.github.com>
2026-02-08 23:13:31 +0530

    frontend: wire cart and delivery routes

--- foodies/src/main.jsx
+++ foodies/src/main.jsx
@@ -1,11 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter } from "react-router-dom";
+import { StoreContextProvider } from "./context/StoreContext.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StoreContextProvider>
+      {/* Client ID  */}
+      <GoogleOAuthProvider clientId="84710312573-omurd96ctpm7js8ltv7t66akra6jgnqd.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </StoreContextProvider>
+  </BrowserRouter>,
+);
